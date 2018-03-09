f03f0e404001b07a02caab05cf33ae9b2996d938
Tomek <tomasz.ciecwierz@gmail.com>
2018-03-09 13:45:58 +0100

    firststep

---
+++ src/Screen.java
@@ -0,0 +1,10 @@
+public class Screen {
+
+    public static void main(String args[]) {
+        String screenSize = "20 10";         //width, heigth
+        String firstReactangle = "3 2 5 6";  //firstCordOfCornet, secondCordOfCorner, width, heigth
+        String secondReactangle = "3 3 10 2";
+        String amountOfReactangle = "2";
+
+    }
+}
